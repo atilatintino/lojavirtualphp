f9321447e5b2d22efef038124320851c22ef97e9
unknown <Digital@DH-A10-40.dhbrasil.digitalhouse.com>
2019-09-27 11:57:13 -0300

    Primeira aula PHP

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+    $nome = "Tintino";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link href="css/style.css" rel="stylesheet">
+    <title>Document</title>
+</head>
+<body>
+        <header class="d-flex justify-content-between align-items-center p-3">
+            <div id="logo">
+                <h1>
+                    <?php 
+                        echo $nome;
+                    ?>
+                </h1>
+            </div>
+            <nav>
+                <ul class="nav d-flex justify-content-between">
+                    <li class="nav-item">Cursos</li>
+                    <li class="nav-item">Login</li>
+                    <li class="nav-item">Cadastrar</li>
+                </ul>
+        </header>
+</body>
+</html>
