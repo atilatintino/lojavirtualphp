8307f8e0101c2c3765fe6eb56a60beec461eeffe
Átila Tintino <atilatintino@gmail.com>
2019-10-02 12:00:50 -0300

    Adicionando Loop no PHP

--- index.php
+++ index.php
@@ -1,6 +1,19 @@
 <?php
     $nomeSistema = "Cursos Ai!";
     $usuario = ["nome"=>"Atila"];
+
+    $produtos = [
+        ["nome"=>"Curso Fullstack", "preco"=>1200.00, "duracao"=>"5 meses"],
+        ["nome"=>"Curso Marketing", "preco"=>1000.00, "duracao"=>"4 meses"],
+        ["nome"=>"Curso Produtos Digitais", "preco"=>1300.00, "duracao"=>"4 meses"],
+        ["nome"=>"Curso C#", "preco"=>1100.00, "duracao"=>"5 meses"],
+        ["nome"=>"Curso História", "preco"=>1400.00, "duracao"=>"6 meses"],
+        ["nome"=>"Curso Matemática", "preco"=>1600.00, "duracao"=>"4 meses"],
+        ["nome"=>"Curso Ciências", "preco"=>1700.00, "duracao"=>"3 meses"],
+        ["nome"=>"Curso Ciências", "preco"=>1700.00, "duracao"=>"3 meses"],
+    ];
+
+    $categorias = ["Cursos", "Palestras", "Artigos", "Cursos", "Palestras", "Artigos", "Cursos", "Palestras", "Artigos"];
 ?>
 
 <!DOCTYPE html>
@@ -42,36 +55,33 @@
                 </ul>
         </header>
         <main>
+            <div class="navbar d-flex justify-content-between">
+                <?php foreach($categorias as $categoria){?>
+                    <ul class="nav">
+                        <li class="nav-item"><?php echo $categoria; ?></li>
+                    </ul>
+                <?php } ?>    
+            </div>            
             <section class="container mt-4">
             <div class="row justify-content-between align-items-center">
-                <div class="col-lg-3 card text-center">
-                    <h5 class="card-title">Título do card</h5>
-                    <img class="card-img-top" src="img/img-1.svg" alt="Imagem de capa do card">
-                    <div class="card-body">
-                        <p class="card-text">R$15,00</p>
-                        <a href="#" class="btn btn-primary">Buy</a>
+
+            <?php if (isset($produtos) && $produtos != []) { ?>
+                <?php foreach($produtos as $produto){ ?>
+
+                    <div class="col-lg-3 card text-center">
+                        <h5 class="card-title"><?php echo $produto['nome']; ?></h5>
+                        <img class="card-img-top" src="img/img-1.svg" alt="Imagem de capa do card">
+                        <div class="card-body">
+                            <p class="card-text"><?php echo $produto['preco'] ?></p>
+                            <a href="#" class="btn btn-primary">Buy</a>
+                        </div>
                     </div>
-                </div>
-            
-            
-                <div class="col-lg-3 card text-center">
-                    <h5 class="card-title">Título do card</h5>
-                    <img class="card-img-top" src="img/img-1.svg" alt="Imagem de capa do card">
-                    <div class="card-body">
-                        <p class="card-text">R$15,00</p>
-                        <a href="#" class="btn btn-primary">Buy</a>
-                    </div>
-                </div>
-            
-            
-                <div class="col-lg-3 card text-center">
-                    <h5 class="card-title">Título do card</h5>
-                    <img class="card-img-top" src="img/img-1.svg" alt="Imagem de capa do card">
-                    <div class="card-body">
-                        <p class="card-text">R$15,00</p>
-                        <a href="#" class="btn btn-primary">Buy</a>
-                    </div>
-                </div>
+
+                <?php } ?>
+            <?php } else { ?>
+                <h1>Não tem produtos cadastrados nessa sessão.</h1>
+            <?php } ?>
+                
             </div>
             </section>
         </main>
